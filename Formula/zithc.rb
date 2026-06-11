6440e78a4afc7912988260ab39a66bc0301455d4
DevHaze <diogoalexandredecodasilvapt@gmail.com>
2026-06-11 14:35:52 +0100

    Add resources for mio and tomlplusplus
    Add resources mio and tomlplusplus with URLs and SHA256 checksums.

--- Formula/zithc.rb
+++ Formula/zithc.rb
@@ -8,6 +8,16 @@ class Zithc < Formula
 
   depends_on "cmake" => :build
 
+  resource "mio" do
+    url "https://github.com/vimpunk/mio/archive/8b6b7d878c89e81614d05edca7936de41ccdd2da.tar.gz"
+    sha256 "86248113bb2f1484f9cd44a260fe09beaa911307073c6f21fa9e765588d54b4b"
+  end
+
+  resource "tomlplusplus" do
+    url "https://github.com/marzer/tomlplusplus/archive/refs/tags/v3.4.0.tar.gz"
+    sha256 "8517f65938a4faae9ccf8ebb36631a38c1cadfb5efa85d9a72e15b9e97d25155"
+  end
+
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
